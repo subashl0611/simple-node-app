9b828c27501cccc20026f4241c6907a3c5378419
Subash <subash.loganathan@zapcg.com>
2022-08-30 13:37:20 +0530

    initial code implementation.

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.use("/", (req, res) => {
+  res.send("Hello world");
+});
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
